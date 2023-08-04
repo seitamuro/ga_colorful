8c8a77a4de26e3512204f0a402901a8f62c01eab
seitamuro <seitamuro0210@gmail.com>
2023-08-04 23:51:36 +0900

    GAの最低限の仕組みを実装

--- sketch.ts
+++ sketch.ts
@@ -1,40 +1,95 @@
 import p5 from 'p5'
 
-const sketch = (p: p5) => {
-    let cx = 100
-    let cy = 100
-    let sx = 200
-    let sy = 200
-    let cvx = p.random(1, 2) * p.random([-1, 1])
-    let cvy = p.random(1, 2) * p.random([-1, 1])
-    let svx = p.random(1, 2) * p.random([-1, 1])
-    let svy = p.random(1, 2) * p.random([-1, 1])
+class Blob {
+    x: number = 0
+    y: number = 0
+    r: number = 0
+    g: number = 0
+    b: number = 0
+    vx: number = 0
+    vy: number = 0
 
+    constructor(x: number, y: number, r: number, g: number, b: number, vx: number, vy: number) {
+        this.x = x
+        this.y = y
+        this.r = r
+        this.g = g
+        this.b = b
+        this.vx = vx
+        this.vy = vy
+    }
+
+    crossover(blob: Blob): Blob {
+        return new Blob(
+            this.x,
+            this.y,
+            (this.r + blob.r) / 2,
+            (this.g + blob.g) / 2,
+            (this.b + blob.b) / 2,
+            0,
+            0
+        )
+    }
+
+    mutation() {
+        this.r = this.mutation_with_clip(this.r);
+        this.g = this.mutation_with_clip(this.g);
+        this.b = this.mutation_with_clip(this.b);
+    }
+
+    mutation_with_clip(value: number) {
+        value += Math.random() * 0.1;
+        if (value < 0) {
+            value = 0;
+        }
+
+        if (value > 1) {
+            value = 1;
+        }
+
+        return value;
+    }
+}
+
+const fitness = (blob: Blob): number => {
+    return blob.r + blob.g + blob.b;
+}
+
+const sketch = (p: p5) => {
+    let blobs: Blob[] = [];
     p.setup = () => {
         p.createCanvas(400, 400)
+        blobs.push(new Blob(0, 0, 0, 0, 0, 0, 0));
+        blobs.push(new Blob(0, 0, 0, 0, 0, 0, 0));
     }
 
     p.draw = () => {
-        p.background(220)
-        p.ellipse(cx, cy, 50, 50)
-        p.rect(sx, sy, 50, 50)
+        let cnt = 0;
+        blobs.forEach(element => {
+            p.fill(element.r * 255, element.g * 255, element.b * 255);
+            p.rect(cnt % 10 * 40, Math.floor(cnt / 10) * 40, 40, 40);
+            cnt += 1;
+        });
+    }
 
-        if (sx < 0 || sx > p.width) {
-            svx *= -1
+    p.mouseClicked = () => {
+        const parent1 = blobs[Math.floor(Math.random() * blobs.length)];
+        const parent2 = blobs[Math.floor(Math.random() * blobs.length)];
+        const child = parent1.crossover(parent2);
+        child.mutation();
+        blobs.push(child);
+        if (blobs.length > 100) {
+            let min_idx = 0;
+            let min_fitness = fitness(blobs[min_idx]);
+            blobs.forEach(element => {
+                const val: number = fitness(element);
+                if (val < min_fitness) {
+                    min_fitness = val;
+                    min_idx = blobs.indexOf(element);
+                }
+            })
+            blobs = blobs.filter((_, idx) => idx != min_idx);
         }
-        if (sy < 0 || sy > p.height) {
-            svy *= -1
-        }
-        sx += svx
-        sy += svy
-        if (cx < 0 || cx > p.width) {
-            cvx *= -1
-        }
-        if (cy < 0 || cy > p.height) {
-            cvy *= -1
-        }
-        cx += cvx
-        cy += cvy
     }
 }
 
